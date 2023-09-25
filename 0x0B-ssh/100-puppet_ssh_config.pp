d61336034b6335a0ffa29f466879f7ec7e11c9f6
gashug <geoffrey.gachuiri98@gmail.com>
2023-09-25 05:56:15 -0700

    Added tasks

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,18 +1,19 @@
-file { '/etc/ssh/sshd_config':
-  ensure  => file,
-  content => epp('ssh/sshd_config.epp'),
-  mode    => '0644',
-  owner   => 'root',
-  group   => 'root',
-  require => Package['openssh-server'],
-  notify  => Service['sshd'],
-}
+# Puppet manifest to configure SSH client
 
 file { '/etc/ssh/ssh_config':
-  ensure  => file,
-  content => epp('ssh/ssh_config.epp'),
-  mode    => '0644',
-  owner   => 'root',
-  group   => 'root',
-  require => Package['openssh-client'],
+  ensure => file,
+}
+
+# Ensure SSH client uses the private key ~/.ssh/school
+file_line { 'Set IdentityFile':
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+  match  => '^#?IdentityFile\s+~/.ssh/school$',
+}
+
+# Ensure SSH client refuses to authenticate using a password
+file_line { 'Disable PasswordAuthentication':
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  match  => '^#?PasswordAuthentication\s+no$',
 }
