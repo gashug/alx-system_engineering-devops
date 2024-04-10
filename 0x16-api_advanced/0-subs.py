c8b75b10378e0b36494b28d187beabfa857cb55a
gashug <geoffrey.gachuiri98@gmail.com>
2024-04-10 00:55:03 -0700

    I still try with 0-subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,24 +11,12 @@ def number_of_subscribers(subreddit):
     """
     import requests
 
-    CLIENT_ID = '4nnh-n-k10tN0YReY9iz9g'
-    SECRET_KEY = '2dyvCWMw8dKRYq2cCPsxQiHWlwZO-Q'
-    auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
-    data = {
-        'grant_type': 'password',
-        'username': 'Acrobatic-Donut-9369',
-        'password': 'gg438379AP!'
-        }
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    res = requests.post('https://www.reddit.com/api/v1/access_token',
-                        auth=auth, data=data, headers=headers)
-    TOKEN = res.json()['access_token']
-    headers['Authorization'] = f'bearer {TOKEN}'
-    url = f'https://oauth.reddit.com/r/{subreddit}/about'
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        about_dict = response.json()
-        return about_dict['data']['subscribers']
+    output = requests.get("https://www.reddit.com/r/{}/about.json"
+                          .format(subreddit),
+                          headers={"User-Agent": "My-User-Agent"},
+                          allow_redirects=False)
+    if output.status_code == 200:
+        about_dict = output.json()
+        return about_dict.get('data').get('subscribers')
     else:
         return 0
